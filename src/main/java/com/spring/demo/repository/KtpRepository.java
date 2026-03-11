6f4398cc0ecb2f8d2905f3ab51553fdc1c8cd8d9
icad08 <irsyadrm08@gmail.com>
2026-03-12 00:42:58 +0700

    add code in ktp repository

--- src/main/java/com/spring/demo/repository/KtpRepository.java
+++ src/main/java/com/spring/demo/repository/KtpRepository.java
@@ -1,4 +1,8 @@
 package com.spring.demo.repository;
 
-public interface KtpRepository {
+import com.spring.demo.model.Ktp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KtpRepository extends JpaRepository<Ktp, Integer> {
+    boolean existsByNomorKtp(String nomorKtp);
 }
